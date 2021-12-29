beb13c6796bb7211635aefc69cd62a8275c7241b
Seth Orell <seth.orell@gmail.com>
2021-12-29 14:19:01 -0600

    Update readme; change deploy bucket name; start on top-three placed test

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This set of data models comes from Alex DeBrie's [2019 re:Invent talk](https://youtu.be/DIQVJqiSUkE) "Data modeling with Amazon DynamoDB (CMY304)"
 
 ## To Deploy and Run
-First, do what need to have CLI access to your AWS account (you can quickly verify this with `aws s3 ls` or similar). The serverless file does nothing else but create the DynamoDB table.
+First, do whatever is necessary to have CLI access to your AWS account (you can quickly verify this with `aws s3 ls` or similar). The serverless file does nothing else but create the DynamoDB table.
 
 `$ npm ci`
 
--- test/getTopThreePlacedOrders.int.test.ts
+++ test/getTopThreePlacedOrders.int.test.ts
@@ -1,4 +1,19 @@
+// import retry from 'async-retry';
+// import ECommerceModels from '../src/dbModels';
+
+// import { Order } from '../src/models';
+// import OrdersRepository from '../src/ordersRepository';
+// import { injectOrder, removeOrders } from './dbUtils';
+// import { OrderBuilder } from './modelBuilders';
+
 describe('When getting top three placed orders', () => {
+  // const testOrders: Order[] = [];
+  // const sut = new OrdersRepository();
+
+  // afterAll(async () => {
+  //   await removeOrders(testOrders);
+  // });
+
   describe('with only one placed order in system', () => {
     it.todo('should return that order');
   });
@@ -6,4 +21,9 @@ describe('When getting top three placed orders', () => {
   describe('with more than three placed orders in system', () => {
     it.todo('should return only three of those orders');
   });
+
+  // const injectTestOrder = async (testOrder: Order): Promise<void> => {
+  //   await injectOrder(testOrder);
+  //   testOrders.push(testOrder);
+  // };
 });
